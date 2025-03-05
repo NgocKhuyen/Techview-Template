7978135ede44a1dcafe0bb3646d25e7a93dd040e
NgocKhuyen <ngockhuyen2k2@gmail.com>
2025-03-05 16:32:54 +0700

    add footer

--- asset/js/index.js
+++ asset/js/index.js
@@ -8,6 +8,7 @@ showSlides();
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
+    if (slides.length === 0) return; 
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -16,3 +17,22 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+// hidden password
+document.addEventListener("DOMContentLoaded", function () {
+    let toggleIcons = document.querySelectorAll(".toggle-password");
+
+    toggleIcons.forEach(icon => {
+        icon.addEventListener("click", function () {
+            let input = document.getElementById(this.getAttribute("data-target"));
+
+            if (input.type === "password") {
+                input.type = "text";
+                this.classList.replace("fa-eye", "fa-eye-slash");
+            } else {
+                input.type = "password";
+                this.classList.replace("fa-eye-slash", "fa-eye");
+            }
+        });
+    });
+});
