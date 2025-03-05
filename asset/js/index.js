493c3eceb405d65af35f350bde6796642fcc4765
NgocKhuyen <ngockhuyen2k2@gmail.com>
2025-03-05 12:58:15 +0700

    add slider

--- asset/js/index.js
+++ asset/js/index.js
@@ -2,3 +2,17 @@
 document.querySelector(".header__menu-category").addEventListener("click", function () {
     document.querySelector(".header__list").classList.toggle("active");
 });
+// slider 
+let slideIndex = 0;
+showSlides();
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
